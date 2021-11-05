be146e1a8d24b357e54ef7633c3cd4fe5b2863a5
Thalles Rodrigues <tcarreiro.rodrigues@gmail.com>
2021-11-05 19:06:49 -0300

    Commit inicial

---
+++ src/services/mqtt/ConnectMQTT.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Connector } from 'mqtt-react-hooks';
+import Status from './Status';
+
+export default function ConnectMQTT() {
+  return (
+    <Connector brokerUrl="ws://192.168.1.165:1884" options={{"username":"mosquitto_user", "password":"mqtt099812"}}>
+      <Status />
+    </Connector>
+  );
+}
